483f746975a836ad438db8c7373456e932a8674d
Illia Osmanov <illia.osmanov@stfalcon.com>
2019-09-24 13:38:31 +0300

    fix some changes

--- src/mutation.types.js
+++ src/mutation.types.js
@@ -2,6 +2,7 @@ export const SET = "SET";
 export const CREATE = "CREATE";
 export const UPDATE = "UPDATE";
 export const REMOVE = "REMOVE";
+export const RESET = "RESET";
 
 export const UPDATE_METADATA = "UPDATE_METADATA";
 export const UPDATE_LOADER = "UPDATE_LOADER";
